66a79a6b9d153e13b5cc796d1c897ed42845bee3
s1120341-hue <s1120341@dmhs.kh.edu.tw>
2025-09-18 07:08:40 +0000

    hw2-1

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <string> // 使用 std::string 需要這個
-int main() {
-  char grade = 'A';            // char：單一字元，用「單引號」
-//   char initial = 'BC';      // ⚠️ 這是多字元常值：要嘛編譯錯、要嘛實作定義（不推薦）
+using namespace std;
 
-  std::string initials = "BC"; // 多個字元請用 string，且用「雙引號」
-  std::cout << "grade=" << grade << ", initials=" << initials << "\n";
+int main() {
+  double fahrenheit;
+  double celsius;
+  cout << "請輸入華氏溫度:\n";
+  cin >> fahrenheit;
+  celsius =(fahrenheit-32)/9*5;
+  cout <<"攝氏溫度是:" <<celsius;
   return 0;
 }
